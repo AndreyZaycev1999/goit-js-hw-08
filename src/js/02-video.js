6eb42e3b528f3bcda143b70d41425f25c349fd90
AndreyZaycev1999 <zaycev.andrey.1999@gmail.com>
2023-03-07 17:25:06 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,43 +4,20 @@ import throttle from "lodash.throttle";
 const vimeoPlayer = document.querySelector('#vimeo-player');
 const player = new Player(vimeoPlayer);
 
-let time = 0;
-
 updateTime();
 
 var throttled = throttle(saveTime, 1000);
 
 player.on('timeupdate', throttled);
 
-function saveTime() {
-    time += 1;
-
-    localStorage.setItem("videoplayer-current-time", time);
-
-    console.log(time);
-    // updateTime();
+function saveTime(e) {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
 };
 
 function updateTime() {
     const outputTime = localStorage.getItem("videoplayer-current-time");
-    console.log(outputTime);
 
     if (outputTime) {
-        time = Number(outputTime);
-
-        player.setCurrentTime(Number(outputTime)).catch(function(error) {
-        switch (error.name) {
-            case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
-                break;
-    
-            default:
-                // some other error occurred
-                break;
-        }
-        });
-        return;
-    }
-
-    
+        player.setCurrentTime(outputTime);
+    };
 };
